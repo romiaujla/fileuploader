4691951a404aac0f202ad2e6ad9bbfeafb13c752
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-08-01 17:16:29 -0400

    Completed FilterOptions and SearchBox Component

--- src/FilterOptions/FilterOptions.js
+++ src/FilterOptions/FilterOptions.js
@@ -6,7 +6,26 @@ export default class FilterOprions extends React.Component {
     render(){
         return (
             <div className='FilterOptions'>
-                FilterOptions
+                <div className='FilterOptions__option'>
+                    <label htmlFor='filter_all'>
+                        <input type='radio' valut='All' id='filter_all' name='filter_all' /> All
+                    </label>
+                </div>
+                <div className='FilterOptions__option'>
+                    <label htmlFor='filter_uploaded'>
+                        <input type='radio' value='Uploaded' id='filter_uploaded' name='filter_uploaded' /> Uploaded
+                    </label>
+                </div>
+                <div className='FilterOptions__option'>
+                    <label htmlFor='filter_synced'>
+                        <input type='radio' value='Synced' id='filter_synced' name='filter_synced' /> Synced
+                    </label>
+                </div>
+                <div className='FilterOptions__option'>
+                    <label htmlFor='filter_new'>
+                        <input type='radio' value='New' id='filter_new' name='filter_new' /> New
+                    </label>
+                </div>
             </div>
         );
     }
--- src/SearchBox/SearchBox.js
+++ src/SearchBox/SearchBox.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './SearchBox.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 export default class SearchBox extends React.Component {
     
     render(){
         return (
             <div className='SearchBox'>
-
+                <FontAwesomeIcon icon={faSearch} />
+                <input placeholder='Search Item' />
             </div>
         );
     }
