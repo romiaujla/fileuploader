e710fb3a3312c63bc537d89b122b854190c04fe2
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-08-01 17:31:14 -0400

    Completed the ControlBar Componenet

--- src/ControlBar/ControlBar.js
+++ src/ControlBar/ControlBar.js
@@ -1,11 +1,19 @@
 import React, {Component} from 'react';
 import './ControlBar.css';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {
+    faRedoAlt,
+    faTrashAlt,
+    faCloudDownloadAlt
+} from '@fortawesome/free-solid-svg-icons';
 
 export default class ControlBar extends Component {
     render(){
         return (
             <div className='ControlBar'>
-                ControlBar
+                <div className='ControlBar__btn'><FontAwesomeIcon icon={faRedoAlt} /></div>
+                <div className='ControlBar__btn'><FontAwesomeIcon icon={faTrashAlt} /></div>
+                <div className='ControlBar__btn'><FontAwesomeIcon icon={faCloudDownloadAlt} /></div>
             </div>
         );
     }
