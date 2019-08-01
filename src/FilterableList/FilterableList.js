943356a9b91d7e0835f664a0f94a7b069f6b44e3
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-08-01 17:27:10 -0400

    Completed ListItem Component

--- src/FilterableList/FilterableList.js
+++ src/FilterableList/FilterableList.js
@@ -3,10 +3,18 @@ import './FilterableList.css';
 import ListItem from '../ListItem/ListItem';
 
 export default class FilterableList extends React.Component {
+
+    static defaultProps = {
+        files: []
+    };
+
     render(){
+
+        const list = this.props.files.map((file, key) => <ListItem {...file} key={key} />)
+
         return (
             <div className='FilterableList'>
-                <ListItem />
+                {list}
             </div>
         )
     }
--- src/ListItem/ListItem.js
+++ src/ListItem/ListItem.js
@@ -1,13 +1,51 @@
 import React, {Component} from 'react';
 import './ListItem.css';
 import ControlBar from '../ControlBar/ControlBar';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {
+    faFileImage,
+    faFileAudio,
+    faFileAlt,
+    faFileVideo
+} from '@fortawesome/free-solid-svg-icons';
 
 export default class ListItem extends Component {
+    
+    static defaultProps = {
+        'fileType' : '',
+        'size' : '',
+        'name' : '',
+        'status' : ''
+    }
+
     render(){
+
+        const icons = {
+            'jpg': faFileImage,
+            'mov': faFileVideo,
+            'txt': faFileAlt,
+            'mp3': faFileAudio
+        }
+
         return (
             <div className='ListItem'>
-                ListItem
-                <ControlBar />
+                <div className='ListItem__icon'>
+                    <div className='ListItem__circle'>
+                        <FontAwesomeIcon icon={icons[this.props.fileType]} />
+                    </div>
+                </div>
+                <div className='ListItem__content'>
+                    <div className='ListItem__heading'>
+                        <div className='ListItem__title'>{this.props.name}</div>
+                        <div className='ListItem__size'>{this.props.size}</div>
+                    </div>
+                    <div className='ListItem__Actions'>
+                        <div className='ListItem__status'>
+                            {this.props.status}
+                        </div>
+                        <ControlBar />
+                    </div>
+                </div>
             </div>
         )
     }
